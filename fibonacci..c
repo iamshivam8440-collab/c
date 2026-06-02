16f214bf7c044d49a66a9258a5472a3f52eb8319
iamshivam8440-collab <iamshivam8440@gmail.com>
2026-06-02 15:38:32 +0530

    Add files via upload

---
+++ fibonacci..c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int fibo(int n);
+int fibo(int n)
+{
+	if(n==0)
+	{
+		return 0;
+	}
+	else if(n==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return fibo(n-1)+fibo(n-2);
+	}
+}
+int main()
+{
+	int n,i;
+	printf("Enter the of number terms:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",fibo(i));
+	}
+	return 0;
+}
